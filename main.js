d65baa357e22724fb24a6e52214950de89f3df92
Amlan <sahuamlanranjan@gmail.com>
2024-09-20 11:05:16 +0530

    portfolio file adding

---
+++ main.js
@@ -0,0 +1,14 @@
+// var typed = new typed(".text", {
+//     strings: ["Software Developer", "Web Developer", "Web Designer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
+var typed = new Typed('#element', {
+    strings: ["Software Developer", "Web Developer", "Web Designer"],
+    typeSpeed: 50,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
